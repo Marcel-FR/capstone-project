5e65141e3512b06cf082286f6920259f0267de97
Marcel-FR <j.m.frankerojas@gmail.com>
2023-03-07 10:43:59 +0100

    import API fetch, import useEffect, fetch the API data into the component, add parts of the styling with styled-components, add the styled-components to the return statement to create a list

--- pages/bestiary.js
+++ pages/bestiary.js
@@ -1,7 +1,34 @@
 import styled from "styled-components";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { fetchMonsters } from "../resources/lib/dnd-api";
 
 export default function BestiaryPage() {
+  const [monsters, setMonsters] = useState([]);
+
+  useEffect(() => {
+    fetchMonsters().then((data) => setMonsters(data));
+  }, []);
+
+  // The useEffect code above was partly taken from stackoverflow
+
+  const Heading = styled.h1`
+    text-align: center;
+  `;
+
+  const MonsterList = styled.ul`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  `;
+
+  const MonsterName = styled.li`
+    border: 1px solid black;
+    width: 100%;
+    padding: 10px;
+    text-align: left;
+  `;
+
   return (
     <>
       <main>
@@ -9,11 +36,13 @@ export default function BestiaryPage() {
         <Link href="/">
           <button>Home</button>
         </Link>
+        <MonsterList>
+          {monsters.map((monster) => (
+            <MonsterName key={monster.index}>{monster.name}</MonsterName>
+          ))}
+        </MonsterList>
+        {console.log("${monsters.length} monsters listed")}
       </main>
     </>
   );
 }
-
-const Heading = styled.h1`
-  text-align: center;
-`;
