42a2a24ae2cc16f9b2afbcf6a8a2b0461e541ad5
Marcel-FR <j.m.frankerojas@gmail.com>
2023-03-10 16:03:27 +0100

    add StyledButton to Homescreen index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import styled from "styled-components";
 import Link from "next/link";
 
+const StyledButton = styled.a`
+  padding: 10px 20px;
+  border-radius: 5px;
+  background-color: #ccc;
+  text-decoration: none;
+  color: #333;
+`;
+
 export default function Home() {
   return (
     <>
@@ -8,7 +16,7 @@ export default function Home() {
         <Heading>Home</Heading>
       </main>
       <Link href="/bestiary">
-      Bestiary
+        <StyledButton>Bestiary</StyledButton>
       </Link>
     </>
   );
