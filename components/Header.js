fc89f40671bc55b22045ee37552dc5516fb8a447
Marcel-FR <j.m.frankerojas@gmail.com>
2023-03-17 11:59:00 +0100

    import Link, useRouter and the SVGs, style the SVGs and the title to fit in the Header

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,57 @@
-import styled from "styled-components"
+import Link from "next/link";
+import styled from "styled-components";
+import { useRouter } from "next/router";
+import { backArrowIcon, searchIcon } from "../assets/svgFiles";
+
 
 const StyledHeader = styled.header`
   position: relative;
   width: 375px;
   height: 80px;
-  color: #AC2023;
-  background-color: #AC2023;
+  color: #ac2023;
+  background-color: #ac2023;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
 `;
 
 const StyledTitle = styled.h1`
   text-align: center;
-  color: #F5E5C3;
+  color: #f5e5c3;
+  margin: 0 5px 0 0;
 `;
 
-export const Header = ({ pageTitle }) => (
-  <StyledHeader>
-    <StyledTitle>{pageTitle}</StyledTitle>
-   </StyledHeader>
-);
+const StyledBackButton = styled.button`
+  background-color: transparent;
+  border: none;
+  outline: none;
+`;
+
+const Icon = styled( backArrowIcon)`
+  fill: #f5e5c3;
+  margin: 5px 0 0 15px;
+  vertical-align: middle;
+`;
+
+
+const Search = styled( searchIcon)`
+  fill: #f5e5c3;
+  margin: 0 20px 0 0;
+  vertical-align: middle;
+`;
+
+export const Header = ({ pageTitle }) => {
+  const router = useRouter();
+
+  return (
+    <StyledHeader>
+      <Link href="/">
+        <StyledBackButton>
+          <Icon onClick={() => router.push("/")} />
+        </StyledBackButton>
+      </Link>
+      <StyledTitle>{pageTitle}</StyledTitle>
+        <Search />
+    </StyledHeader>
+  );
+};
