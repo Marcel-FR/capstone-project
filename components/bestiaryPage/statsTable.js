e50397fc2a0bde5a88bf57ea7b459fda1ef43834
Marcel-FR <j.m.frankerojas@gmail.com>
2023-03-20 10:58:17 +0100

    restructure code in the statsTable.js

--- components/bestiaryPage/statsTable.js
+++ components/bestiaryPage/statsTable.js
@@ -1,6 +1,33 @@
 export default function StatsTable(props) {
   const { monster } = props;
 
+  function getArmorClass(monster) {
+    if (!monster?.armor_class?.length) return "";
+    const { value = "", type = "" } = monster.armor_class[0] || {};
+    return `${value}${type ? ` (${type})` : ""}`;
+  }
+
+// The code above returns the armor class of a monster if available. If it's not available, it will return an empty string
+
+function getSpeed(monster) {
+  const { walk = "", swim = "", hover = "" } = monster.speed || {};
+  let speedString = "";
+  if (walk) {
+    speedString += `Walk: ${walk === undefined ? '-' : walk}`;
+    if (swim !== undefined || hover !== undefined) speedString += ", ";
+  }
+  if (swim) {
+    speedString += `Swim: ${swim}`;
+    if (hover) speedString += ", ";
+  }
+  if (hover) {
+    speedString += `Hover: ${hover}`;
+  }
+  return speedString;
+}
+
+//The code above is a function to create a string dor the monster's speed data, which concatenates different types of speed (walking, swimming, hovering)
+
   return (
     <div className="stats-table">
       <div>
@@ -71,30 +98,3 @@ export default function StatsTable(props) {
     </div>
   );
 }
-
-function getArmorClass(monster) {
-  if (!monster?.armor_class?.length) return "";
-  const { value = "", type = "" } = monster.armor_class[0] || {};
-  return `${value}${type ? ` (${type})` : ""}`;
-}
-
-// The code above returns the armor class of a monster if available. If it's not available, it will return an empty string
-
-function getSpeed(monster) {
-  const { walk = "", swim = "", hover = "" } = monster.speed || {};
-  let speedString = "";
-  if (walk) {
-    speedString += `Walk: ${walk === undefined ? '-' : walk}`;
-    if (swim !== undefined || hover !== undefined) speedString += ", ";
-  }
-  if (swim) {
-    speedString += `Swim: ${swim}`;
-    if (hover) speedString += ", ";
-  }
-  if (hover) {
-    speedString += `Hover: ${hover}`;
-  }
-  return speedString;
-}
-
-//The code above is a function to create a string dor the monster's speed data, which concatenates different types of speed (walking, swimming, hovering)
