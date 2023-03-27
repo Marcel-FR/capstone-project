a3cdc426523682f6421485f09c9e8b74d5b378bb
Marcel-FR <j.m.frankerojas@gmail.com>
2023-03-27 15:13:06 +0200

    import uid to _app.js and create a function that implements LocalStorage to the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,20 @@
 import GlobalStyle from "@/styles";
 import Head from "next/head";
+import { uid } from "uid";
+import useLocalStorageState from "use-local-storage-state";
 
 export default function App({ Component, pageProps }) {
+  const [formData, setFormData] = useLocalStorageState("formData", {defaultValue:[]})
+  function handleAddFormData(newFormData) {
+    setFormData([...formData, {id: uid(), ...newFormData}])
+  }
   return (
     <>
       <GlobalStyle />
       <Head>
         <title>DnD Companion</title>
       </Head>
-      <Component {...pageProps} />
+      <Component {...pageProps} data={formData} onAddFormData={handleAddFormData} />
     </>
   );
 }
