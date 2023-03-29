2afe5409a18b0936124dd5de1dfd92906524d8d1
Marcel-FR <j.m.frankerojas@gmail.com>
2023-03-29 18:34:02 +0200

    update my whole LocalStorage and handleDeleteMonster funcitons in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,18 +3,47 @@ import Head from "next/head";
 import { uid } from "uid";
 import useLocalStorageState from "use-local-storage-state";
 
+const monsters = require("/resources/lib/5e-SRD-Monsters.json");
+
 export default function App({ Component, pageProps }) {
-  const [formData, setFormData] = useLocalStorageState("formData", {defaultValue:[]})
+  const [monsterData, setMonsterData] = useLocalStorageState("monsterData", {
+    defaultValue: monsters,
+  });
+  const [formData, setFormData] = useLocalStorageState("formData", {
+    defaultValue: [],
+  });
   function handleAddFormData(newFormData) {
-    setFormData(newFormData)
+    setFormData(newFormData);
+    setMonsterData([...monsterData, newFormData]);
   }
+
+  function handleDeleteMonster(id) {
+    setMonsterData(monsterData.filter((monster) => monster.id !== id));
+    console.log(monsterData);
+  }
+  /*  const handleDeleteMonster = (id) => {
+    setMonsterData(monsterData.filter((monster) => monster.id !== id));
+  }; */
+
   return (
     <>
       <GlobalStyle />
       <Head>
-        <title>DnD Companion</title>
+        <title>Delver&apos;s Guide</title>
       </Head>
-      <Component {...pageProps} data={formData} onAddFormData={handleAddFormData} />
+      {monsterData ? (
+        <Component
+          {...pageProps}
+          data={formData}
+          onAddFormData={handleAddFormData}
+          monsterData={[...monsterData].sort((a, b) =>
+            a.name.localeCompare(b.name)
+          )}
+          onDeleteMonster={handleDeleteMonster}
+        />
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   );
 }
