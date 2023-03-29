ce8d1ec22ce84e4af393bf40c1aff4a769025c25
Marcel-FR <j.m.frankerojas@gmail.com>
2023-03-29 18:37:23 +0200

    validate every monster. with monster?. to prevent any errors. Add the delete button to the informationPage.js

--- components/bestiaryPage/informationPage.js
+++ components/bestiaryPage/informationPage.js
@@ -1,3 +1,4 @@
+import { MdDelete } from "react-icons/md";
 import {
   getArmorClass,
   getSpeed,
@@ -12,6 +13,7 @@ import {
   getLegendaryActions,
 } from "./informationPageFunctions";
 import {
+  DeleteWrapper,
   StatBlock,
   OrangeBorder,
   OrangeBorderBottom,
@@ -28,20 +30,34 @@ import {
   AbilitiesP,
   SeparationLine,
 } from "./informationPageStyling";
-import { CloseIcon } from "./informationPageStyling";
 import { useRouter } from "next/router";
+import { CloseIcon } from "../svgFiles";
 
-export default function InformationPage({ monster }) {
+export default function InformationPage({
+  monster,
+  onDeleteMonster,
+  monsterData,
+}) {
+  console.log(monster);
   const router = useRouter();
   return (
     <StatBlock>
       <OrangeBorder />
-      <CreatureHeadingH1>{monster.name}</CreatureHeadingH1>
+      <CreatureHeadingH1>{monster?.name}</CreatureHeadingH1>
+      {monster?.id && (
+        <DeleteWrapper>
+          <MdDelete
+            onClick={() => {
+              onDeleteMonster(monster?.id);
+              router.push("/bestiary");
+            }}
+          />
+        </DeleteWrapper>
+      )}
+      <CloseIcon />
       <CreatureHeadingH2>
-        {monster.size} {monster.type}, {monster.alignment}
+        {monster?.size} {monster?.type}, {monster?.alignment}
       </CreatureHeadingH2>
-      <CloseIcon onClick={() => router.push("/bestiary")} />
-
       <SeparationLine>
         <polyline points="0,0 400,2.5 0,5"></polyline>
       </SeparationLine>
@@ -54,8 +70,8 @@ export default function InformationPage({ monster }) {
         <PropertyLineH4>Hit Points</PropertyLineH4>
         <PropertyLineP>
           {" "}
-          {monster.hit_points}{" "}
-          {monster.hit_dice && <span>({monster.hit_dice})</span>}
+          {monster?.hit_points}{" "}
+          {monster?.hit_dice && <span>({monster?.hit_dice})</span>}
         </PropertyLineP>
       </PropertyLine>
       <PropertyLine>
@@ -70,27 +86,27 @@ export default function InformationPage({ monster }) {
       <Abilities>
         <AbilitiesDiv>
           <AbilitiesH4>STR</AbilitiesH4>
-          <AbilitiesP>{monster.strength}</AbilitiesP>
+          <AbilitiesP>{monster?.strength}</AbilitiesP>
         </AbilitiesDiv>
         <AbilitiesDiv>
           <AbilitiesH4>DEX</AbilitiesH4>
-          <AbilitiesP>{monster.dexterity}</AbilitiesP>
+          <AbilitiesP>{monster?.dexterity}</AbilitiesP>
         </AbilitiesDiv>
         <AbilitiesDiv>
           <AbilitiesH4>CON</AbilitiesH4>
-          <AbilitiesP>{monster.constitution}</AbilitiesP>
+          <AbilitiesP>{monster?.constitution}</AbilitiesP>
         </AbilitiesDiv>
         <AbilitiesDiv>
           <AbilitiesH4>INT</AbilitiesH4>
-          <AbilitiesP>{monster.intelligence}</AbilitiesP>
+          <AbilitiesP>{monster?.intelligence}</AbilitiesP>
         </AbilitiesDiv>
         <AbilitiesDiv>
           <AbilitiesH4>WIS</AbilitiesH4>
-          <AbilitiesP>{monster.wisdom}</AbilitiesP>
+          <AbilitiesP>{monster?.wisdom}</AbilitiesP>
         </AbilitiesDiv>
         <AbilitiesDiv>
           <AbilitiesH4>CHA</AbilitiesH4>
-          <AbilitiesP>{monster.charisma}</AbilitiesP>
+          <AbilitiesP>{monster?.charisma}</AbilitiesP>
         </AbilitiesDiv>
       </Abilities>
 
@@ -115,7 +131,7 @@ export default function InformationPage({ monster }) {
         <PropertyLineH4>Challenge</PropertyLineH4>
         <PropertyLineP>
           {" "}
-          {monster.challenge_rating} ({monster.xp} XP)
+          {monster?.challenge_rating} ({monster?.xp} XP)
         </PropertyLineP>
       </PropertyLine>
 
